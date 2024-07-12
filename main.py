261ffd231a25b265d55e9767bafc848a20da5513
pyqzo6-tejwix-tYxqoc <roman.senkevich@slmax.by>
2024-07-12 23:48:04 +0300

    [+] create password hiden file

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
 import logging
-import os
-from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 from telegram.ext import Application, CommandHandler, CallbackContext
+from passwords import TOKEN, PUBLIC_URL
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Retrieve environment variables
-TOKEN = os.getenv('TOKEN')
-PUBLIC_URL = os.getenv('PUBLIC_URL')
-
-# Logging setup
+# Логирование
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Define the start command handler
+# Функция для команды /start
 async def start(update: Update, context: CallbackContext) -> None:
     keyboard = [
         [InlineKeyboardButton("Open Web App", web_app=WebAppInfo(url=PUBLIC_URL))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text('Hi! Click the button below to open the web app.', reply_markup=reply_markup)
+    await update.message.reply_text('Привет! Нажмите кнопку ниже, чтобы открыть веб-приложение.', reply_markup=reply_markup)
 
-# Main function to run the bot
+# Основная функция для запуска бота
 def main() -> None:
-    app = Application.builder().token(TOKEN).build()  # Create the bot application instance
+    # Создаем экземпляр приложения
+    app = Application.builder().token(TOKEN).build()
 
-    # Add command handler for the /start command
-    app.add_handler(CommandHandler("start", start))
+    # Добавляем обработчик команды /start
+    app.add_handler(CommandHandler("web", start))
 
-    # Start polling for updates
+    # Запускаем бота
     app.run_polling()
 
 if __name__ == '__main__':
